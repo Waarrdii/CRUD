5ee5ea476163313a4e205b7620ebe78cfbbb7e68
waarrdii <waarrdii@gmail.com>
2024-11-11 14:47:52 +0700

    delete all Done

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -8,6 +8,7 @@
                     <th scope="col" class="p-4">
                         <div class="flex items-center">
                             <input id="checkbox-all-search" type="checkbox"
+                                @change="selectAll($event)"
                                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                             <label for="checkbox-all-search" class="sr-only">checkbox</label>
                         </div>
@@ -31,7 +32,8 @@
                     class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <td class="w-4 p-4">
                         <div class="flex items-center">
-                            <input id="checkbox-table-search-1" type="checkbox" v-model="selectedIds"
+                            <input id="checkbox-table-search-1" type="checkbox" 
+                                v-model="selectedIds"
                                 :value="product.id"
                                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                             <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
@@ -99,12 +101,13 @@ const form = useForm({
     ids: [],
 });
 
-const toggleAll = (event) => {
+const selectAll = (event) => {
     if (event.target.checked) {
         selectedIds.value = props.products.map(product => product.id);
     } else {
         selectedIds.value = [];
     }
+    console.log(selectedIds.value);
 };
 
 const selectedIds = ref([]);
