5bad46f5c867345471057eb88aabc24b5e7a40fd
waarrdii <waarrdii@gmail.com>
2024-11-21 12:06:50 +0700

    edit index component tahap 2

--- resources/js/Components/Index.vue
+++ resources/js/Components/Index.vue
@@ -3,6 +3,10 @@ import TableComponent from './Table/TableComponent.vue';
 import TableHead from './Table/TableHead.vue';
 import TableData from './Table/TableData.vue';
 import TableRow from './Table/TableRow.vue';
+import Checkbox from './Checkbox.vue';
+import UrlLink from './UrlLink.vue';
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
 
 import SecondaryTab from './SecondaryTab.vue';
 
@@ -15,6 +19,15 @@ const props = defineProps({
     },
 });
 
+const brands = ref([]);
+const isLoading = ref(true);
+
+onMounted( async () => {
+    const response = await axios.get(route('brands.showAllBrands'));
+    brands.value = response.data;
+    isLoading.value = false;
+})
+
 </script>
 
 <template>
@@ -23,11 +36,27 @@ const props = defineProps({
             <h2>Index</h2>
         </template>
         <template #default>
+            <div v-if="isLoading">
+                Loading data...
+            </div>
 
-            <TableComponent>
+            <TableComponent v-if="brands.length && items.length > 0">
                 <TableHead v-for="column in columns" :key="column">{{ column }}</TableHead>
                 <TableRow v-for="item in items" :key="item.id">
-                    <TableData v-for="column in columns" :key="column">{{ item[column] }}</TableData>
+                    <TableData v-for="column in columns" :key="column">
+                        <template v-if="column === 'name'">
+                            <UrlLink :href="route('products.edit', item.id)">
+                                {{ item.name }}
+                            </UrlLink>
+                        </template>
+                        <template v-else-if="column === 'brand_id'">
+                            {{ brands.find(brand => brand.id === item.brand_id).name }}
+                        </template>
+                           
+                        <template v-else>
+                            {{ item[column] }}
+                        </template>
+                    </TableData>
                 </TableRow>
             </TableComponent>
             
